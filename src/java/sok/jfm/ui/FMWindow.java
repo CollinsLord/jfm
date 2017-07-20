d358b06c243b23020677887e725e9afd1738445d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-07-20 23:21:33 +0000

    Added javadoc comments to FMWindow. Refs #7

--- src/java/sok/jfm/ui/FMWindow.java
+++ src/java/sok/jfm/ui/FMWindow.java
@@ -2,7 +2,14 @@ package sok.jfm.ui;
 
 import javax.swing.JFrame;
 
+/**
+ * This class represents the main file manager window.
+ */
 public class FMWindow extends JFrame {
+
+    /**
+     * Create a new <code>FMWindow</code>.
+     */
     public FMWindow() {
         setTitle("Java File Manager");
     }
