66223415ab55a7c2baaf7949ad58234ca9af36ca
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-07-27 01:26:35 +0000

    Added java doc to App class. Refs #7

--- src/java/sok/jfm/App.java
+++ src/java/sok/jfm/App.java
@@ -2,7 +2,19 @@ package sok.jfm;
 
 import sok.jfm.ui.FMWindow;
 
+/**
+ * The application class.
+ *
+ * This class should only hold the main main method as well as some other
+ * utility stuff used required for starting up the application.
+ */
 public class App {
+
+    /**
+     * Application entry point.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         (new FMWindow()).setVisible(true);
     }
