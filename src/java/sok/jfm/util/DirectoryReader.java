60112fcc620173f18409bd461438b0f0d46905fe
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-03 15:26:39 +0000

    FIX: missing return statement.

--- src/java/sok/jfm/util/DirectoryReader.java
+++ src/java/sok/jfm/util/DirectoryReader.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class DirectoryReader {
     public ArrayList<DirectoryEntry> readDir(String path) {
         // TODO: add code here.
+        return null; // Temporary to get it to compile.
     }
 }
